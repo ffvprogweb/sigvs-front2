4701ec3f1fecbe267e8271982a6a62d78b507086
ffvprogweb <disclabes@gmail.com>
2024-03-19 10:02:24 -0300

    Refatoracao - api

--- src/componentes/ProdutoServico/index.js
+++ src/componentes/ProdutoServico/index.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 /* servico de manipulação de produtos */
-const url =
-  "https://produto-backend2-0bd4ca5d2150.herokuapp.com/api/v1/produtos";
+/*const url = */
 
-/*const url = "http://localhost:8080/api/v1/produtos";*/
+const url = "http://localhost:8080/api/v1/produtos";
 
 export const listaDeProdutos = () => axios.get(url);
 export const cadastroDeProduto = (produto) => axios.post(url, produto);
